abab8e5b4b3e5308cbd9c0405bc4ff312c3c3ad4
Barasa-Micah <barasamica@gmail.com>
2024-07-24 12:17:21 +0300

    Linked the register to the navigation file

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -7,6 +7,7 @@ import ProjectScreen from '../screens/ProjectScreen';
 import ChatScreen from '../screens/ChatScreen';
 import TranslationScreen from '../screens/TranslationScreen';
 import NextScreen from '../screens/NextScreen';
+import RegisterScreen from '../screens/RegisterScreen';
 
 const Stack = createStackNavigator();
 
@@ -15,6 +16,7 @@ export default function AppNavigator() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Welcome">
         <Stack.Screen name="Welcome" component={WelcomeScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="Register" component={RegisterScreen} options={{ headerShown: false }} />
         <Stack.Screen name="Main" component={MainScreen} options={{ headerShown: false }} />
         <Stack.Screen name="Project" component={ProjectScreen} options={{ headerShown: false }} />
         <Stack.Screen name="Chat" component={ChatScreen} options={{ headerShown: false }} />
